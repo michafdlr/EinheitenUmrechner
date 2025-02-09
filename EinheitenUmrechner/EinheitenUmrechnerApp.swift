89d9484bbde36cc35ffc3fc6d7b9e780d5605e87
Michael Fiedler <michael.fiedler1604@gmail.com>
2025-02-09 17:22:12 +0100

    Initial Commit

---
+++ EinheitenUmrechner/EinheitenUmrechnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EinheitenUmrechnerApp.swift
+//  EinheitenUmrechner
+//
+//  Created by Michael Fiedler on 09.02.25.
+//
+
+import SwiftUI
+
+@main
+struct EinheitenUmrechnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
